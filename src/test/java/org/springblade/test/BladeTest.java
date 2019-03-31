2aef76e771f843e19c14b28a7f3913e8387231af
smallchill <smallchill@163.com>
2019-03-31 23:51:39 +0800

    :zap: 增加单元测试

---
+++ src/test/java/org/springblade/test/BladeTest.java
@@ -0,0 +1,24 @@
+package org.springblade.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springblade.core.test.BladeBootTest;
+import org.springblade.core.test.BladeSpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Blade单元测试
+ *
+ * @author Chill
+ */
+@SpringBootTest
+@BladeBootTest("blade-runner")
+@RunWith(BladeSpringRunner.class)
+public class BladeTest {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("测试～～～～～～");
+	}
+
+}

da41f03c45db032533699a38cf13ab2299bc17e4
smallchill <smallchill@163.com>
2019-04-09 00:05:03 +0800

    :zap: 集成druid

--- src/main/java/org/springblade/common/config/BladeConfiguration.java
+++ src/main/java/org/springblade/common/config/BladeConfiguration.java
@@ -40,6 +40,7 @@ public class BladeConfiguration implements WebMvcConfigurer {
 		secureRegistry.excludePathPatterns("/js/**");
 		secureRegistry.excludePathPatterns("/webjars/**");
 		secureRegistry.excludePathPatterns("/swagger-resources/**");
+		secureRegistry.excludePathPatterns("/druid/**");
 		return secureRegistry;
 	}
 

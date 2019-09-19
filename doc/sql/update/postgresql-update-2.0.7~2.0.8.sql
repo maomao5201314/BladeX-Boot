83f599debdb96d1e90ca253cf89ff87260f18dce
smallchill <smallchill@163.com>
2019-09-19 23:39:12 +0800

    :zap:  更新sql

--- doc/sql/update/postgresql-update-2.0.7~2.0.8.sql
+++ doc/sql/update/postgresql-update-2.0.7~2.0.8.sql
@@ -14,3 +14,12 @@ INSERT INTO "blade_role_menu"("id","menu_id","role_id")
 VALUES ('1161272593873322996', '1164733121140551682', '1123598816738675201');
 INSERT INTO "blade_role_menu"("id","menu_id","role_id")
 VALUES ('1161272593873322997', '1164733349637844993', '1123598816738675201');
+
+-- ----------------------------
+-- 重新配置oss字典
+-- ----------------------------
+DELETE FROM "blade_dict" WHERE CODE = 'oss';
+INSERT INTO "blade_dict"("id", "parent_id", "code", "dict_key", "dict_value", "sort", "remark", "is_deleted") VALUES (1123598814738676224, 0, 'oss', -1, '对象存储类型', 6, NULL, 0);
+INSERT INTO "blade_dict"("id", "parent_id", "code", "dict_key", "dict_value", "sort", "remark", "is_deleted") VALUES (1123598814738676225, 1123598814738676224, 'oss', 1, 'minio', 1, NULL, 0);
+INSERT INTO "blade_dict"("id", "parent_id", "code", "dict_key", "dict_value", "sort", "remark", "is_deleted") VALUES (1123598814738676226, 1123598814738676224, 'oss', 2, 'qiniu', 2, NULL, 0);
+INSERT INTO "blade_dict"("id", "parent_id", "code", "dict_key", "dict_value", "sort", "remark", "is_deleted") VALUES (1123598814738676227, 1123598814738676224, 'oss', 3, 'qiniu', 3, NULL, 0);

7deb74e6642ed770b176726a69dbcd7dd8174b1d
smallchill <smallchill@163.com>
2019-11-06 10:14:33 +0800

    :tada: 注释单元测试

--- src/test/java/org/springblade/test/BladeTest.java
+++ src/test/java/org/springblade/test/BladeTest.java
@@ -1,20 +1,15 @@
 package org.springblade.test;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springblade.core.test.BladeBootTest;
-import org.springblade.core.test.BladeSpringRunner;
-
 /**
  * Blade单元测试
  *
  * @author Chill
  */
-@RunWith(BladeSpringRunner.class)
-@BladeBootTest(appName = "blade-runner", profile = "test")
+//@RunWith(BladeSpringRunner.class)
+//@BladeBootTest(appName = "blade-runner", profile = "test")
 public class BladeTest {
 
-	@Test
+	//@Test
 	public void contextLoads() {
 		System.out.println("测试～～～～～～");
 	}

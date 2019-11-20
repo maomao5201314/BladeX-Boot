11892388f75e621c1e1404da2360735c19731c4a
smallchill <smallchill@163.com>
2019-11-20 18:20:49 +0800

    :zap: 优化sql脚本

--- doc/sql/update/postgresql-update-2.2.0~2.2.1.sql
+++ doc/sql/update/postgresql-update-2.2.0~2.2.1.sql
@@ -14,9 +14,9 @@ ALTER TABLE "public"."blade_log_usual"
     ADD COLUMN "method_class" varchar(500),
     ADD COLUMN "method_name" varchar(500);
 
-COMMENT ON COLUMN "public"."blade_dict"."remote_ip" IS '操作IP地址';
-COMMENT ON COLUMN "public"."blade_dict"."method_class" IS '方法类';
-COMMENT ON COLUMN "public"."blade_dict"."method_name" IS '方法名';
+COMMENT ON COLUMN "public"."blade_log_usual"."remote_ip" IS '操作IP地址';
+COMMENT ON COLUMN "public"."blade_log_usual"."method_class" IS '方法类';
+COMMENT ON COLUMN "public"."blade_log_usual"."method_name" IS '方法名';
 
 -- ----------------------------
 -- 插入菜单数据

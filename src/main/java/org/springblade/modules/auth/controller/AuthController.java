d7dfa72aaadc42411998dbd6df2e07c0c80e7f40
smallchill <smallchill@163.com>
2020-02-25 00:17:43 +0800

    :zap: 登出接口增加msg字段

--- src/main/java/org/springblade/modules/auth/controller/AuthController.java
+++ src/main/java/org/springblade/modules/auth/controller/AuthController.java
@@ -102,7 +102,7 @@ public class AuthController {
 	@GetMapping("/oauth/logout")
 	public Kv logout() {
 		BladeUser user = AuthUtil.getUser();
-		return Kv.create().set("success", "true").set("account", user.getAccount());
+		return Kv.create().set("success", "true").set("account", user.getAccount()).set("msg", "success");
 	}
 
 }

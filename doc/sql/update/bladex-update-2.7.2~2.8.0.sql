a40d20792630fe29babce745344a9d48f95023ff
smallchill <smallchill@163.com>
2021-03-01 12:05:58 +0800

    :tada: boot版无需修改swagger地址，改回原sql脚本

--- doc/sql/update/bladex-update-2.7.2~2.8.0.sql
+++
@@ -1,4 +0,0 @@
--- -----------------------------------
--- 修改swagger独立服务接口文档最新地址
--- -----------------------------------
-update blade_menu set path = 'http://localhost:18000/doc.html' where path = 'http://localhost/doc.html';
